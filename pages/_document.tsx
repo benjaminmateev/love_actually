273f8ca4d061f9b08477551be4b67a7fbedce52b
Benjamin Mateev <benjamin.mateev@gmail.com>
2024-08-14 00:09:05 +0200

    upading to next14 to remove all the issues we had so far@

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document () {
   return (
-    <Html lang="en" data-theme="acid">
+    <Html lang="en" data-theme="love-actually">
       <Head />
       <body className="h-full">
         <Main />
